586f257abbeb38f2bd19106686a995a2475c08bf
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-27 22:10:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,8 +13,8 @@ def line(katz_deli)
 end
 
 def take_a_number(line_array, names)
-  
-  puts "Welcome, #{names}. You are number #{line_array} in line."
+  line_array.each do |people_in_line|
+  puts "Welcome, #{people_in_line}. You are number #{line_array} in line."
 end  
 
 def now_serving(katz_deli)
