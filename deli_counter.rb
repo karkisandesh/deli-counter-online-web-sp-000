948b8add4ebeb8ae6e67e5dafca4f0d02f123cf0
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-27 23:35:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 katz_deli = [ ]
 
 def line(katz_deli)
-  count = 1
-  katz_deli.push(katz_deli)
-  if katz_deli.length >= 1
+  if  katz_deli.length >= 1
+      count = 1
+      katz_deli.push(katz_deli)
       katz_deli.each do |name|  
       puts "The line is currently: #{count}. #{name}"
       count += 1
