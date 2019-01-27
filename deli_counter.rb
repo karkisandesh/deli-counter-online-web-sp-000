3767dbe0cfefc92143177e011cdd92d591ddd999
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-27 23:49:07 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,8 +21,8 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length >= 1
-    push(katz_deli)
-    if katz_deli != nill
+      push(katz_deli)
+      if katz_deli != nill
       katz_deli.each do |customer|
       puts "Currently serving #{customer}."
       katz_deli.shift
