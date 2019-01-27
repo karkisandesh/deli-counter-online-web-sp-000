a19a0cddcc35ba55139341fd97e0b043915257e0
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-27 21:43:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 katz_deli = [ ]
 
 def line(katz_deli)
-  katz_deli.each do |num| 
+  katz_deli.each do |num, names| 
      if num >=1
       puts "The line is currently: #{num}. #{names}"
     else 
