ed2c80c9e89c8d62fc649d0321c5006bcbe532d3
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-27 21:31:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,12 @@
 katz_deli = [ ]
 
-def line(katz_deli)
+def line(katz_deli, names)
   katz_deli.each do |num| 
     if num < 0
       puts "The line is currently empty."
   else
     puts "The line is currently: #{num}. #{names}"
+  end
 end
 
 def take_a_number(line_array, names)
